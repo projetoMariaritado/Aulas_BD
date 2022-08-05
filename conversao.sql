1133526a70df3f398327d21bd330ba9f7ef8ddaf
maria rita prado <100208753+projetoMariaritado@users.noreply.github.com>
2022-08-05 09:05:12 -0300

    Add files via upload

---
+++ conversao.sql
@@ -0,0 +1,28 @@
+CREATE database revisao;
+USE revisao;
+
+CREATE TABLE nome_produto (
+codigo int PRIMARY KEY,
+preco decimal(6,2),
+nome_produto vachar(255),
+cor vachar(255)
+);
+
+CREATE TABLE clientes (
+id_cliente int PRIMARY KEY,
+nome_cliente vachar(255),
+email vachar(255),
+estado vachar(2)
+);
+
+CREATE TABLE compras (
+nf int PRIMARY KEY,
+data_compras date,
+id_cliente int,
+codigo int,
+FOREIGN KEY(id_cliente) REFERENCES clientes (id_cliente),
+FOREIGN KEY(codigo) REFERENCES produtos (codigo)
+);
+
+
+
